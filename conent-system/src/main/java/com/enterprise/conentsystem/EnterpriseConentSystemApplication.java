7e87c80c4d7e0312134259353e566fcda0cff53f
manish <soleado707@gmail.com>
2026-02-04 21:35:25 +0000

    Spring Boot application running successfully with Java 17

---
+++ conent-system/src/main/java/com/enterprise/conentsystem/EnterpriseConentSystemApplication.java
@@ -0,0 +1,13 @@
+package com.enterprise.conentsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnterpriseConentSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnterpriseConentSystemApplication.class, args);
+	}
+
+}
---
+++ conent-system/src/test/java/com/enterprise/conentsystem/EnterpriseConentSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enterprise.conentsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnterpriseConentSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
